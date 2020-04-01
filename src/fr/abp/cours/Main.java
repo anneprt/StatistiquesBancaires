4e45fcd19f9fbb5b609525143dbbbe0ffc256d89
Anne <patesdegeek@gmail.com>
2020-04-01 21:29:38 +0200

    saisie des valeurs au clavier

--- src/fr/abp/cours/Main.java
+++ src/fr/abp/cours/Main.java
@@ -1,8 +1,19 @@
 package fr.abp.cours;
 
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner lectureClavier = new Scanner(System.in);
+        System.out.print("Nombre de paiements par Carte Bleue");
+        nbCb = lectureClavier.nextInt();
+        System.out.print("Nombre de chèques émis");
+        nbCheque = lectureClavier.nextInt();
+        System.out.print("Nombre de virements automatiques effectués");
+        nbVirements = lectureClavier.nextInt();
 
 
     }
